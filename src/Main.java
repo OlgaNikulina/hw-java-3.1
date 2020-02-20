224d8542a9e4bca5acefe6283ad8397d746ce23b
OlgaNikulina <nikulinaov@inbox.ru>
2020-02-20 12:46:18 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        long price = 20020;
+        int mile = 20;
+        long total = price / mile;
+        System.out.println(total);
+    }
+}
+
